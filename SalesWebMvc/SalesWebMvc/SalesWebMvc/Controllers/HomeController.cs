2b15a9100d3de0147730e48e8f034811010be257
Marco Rodrigues <marco.rodrigues@pmm.am.gov.br>
2020-02-03 15:37:57 -0400

    First controller and Razor pages tests

--- SalesWebMvc/SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Wem MVC App from C# Course.";
+            ViewData["Aluno"] = "marco.rodrigues@pmm.am.gov.br";
 
             return View();
         }
